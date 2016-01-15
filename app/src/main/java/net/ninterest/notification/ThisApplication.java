db99c67e8523e76de0c16471e8fa6053fbd43a95
Shun Sugai <sugaishun@gmail.com>
2016-01-15 23:17:25 +0900

    Start tracking with GoogleAnalytics

--- app/src/main/java/net/ninterest/notification/ThisApplication.java
+++ app/src/main/java/net/ninterest/notification/ThisApplication.java
@@ -2,15 +2,32 @@ package net.ninterest.notification;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.helpshift.Core;
 import com.helpshift.support.Support;
 
 public class ThisApplication extends Application {
 
+    private Tracker mTracker;
+
     @Override
     public void onCreate() {
         super.onCreate();
         initHelpShift();
+    }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            mTracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return mTracker;
     }
 
     private void initHelpShift() {
